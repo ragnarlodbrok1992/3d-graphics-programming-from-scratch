921b066af643de261ae5243cf5e94bceba0d3ab7
ragnarlodbrok1992 <maraus92@gmail.com>
2025-06-20 23:15:13 +0200

    WIP

--- src/tools/tools_main.c
+++ src/tools/tools_main.c
@@ -12,10 +12,21 @@ typedef enum {
 #define OPTIONS_OK 0
 #define ARGUMENTS_ERROR 1
 
+#define FILE_OK 0
+#define FILE_NOT_OK 1
+#define FILE_NOT_OBJ 2
+
 typedef struct {
   SelectedLib selected_lib;
   char* input_file_name;
 } options_t;
+
+typedef struct {
+  char* mtllib_file;
+  char* object_name; // Assuming one object name per file - triangulated mesh export from bforartists
+  int num_verts;
+  int num_faces;
+} fileinfo_t;
 
 void print_help_message() {
   printf("Usage: render_tools [ARGUMENTS]\n");
@@ -28,6 +39,33 @@ void print_help_message() {
 }
 
 static options_t* main_options;
+static FILE* obj_file;
+
+int parse_file(char* filename) {
+  // Checking file opening
+  errno_t obj_file_err = fopen_s(&obj_file, filename, "r");
+  printf("obj_file_err: %d, obj_file: %p\n", obj_file_err, obj_file);
+
+  if (!(obj_file_err == 0)) return FILE_NOT_OK;
+
+  // TEST - print file line by line
+  char buffer[1024];
+
+  while (fgets(buffer, sizeof(buffer), obj_file) != NULL) {
+    printf("%s", buffer);
+    // printf("position: %ld\n", ftell(obj_file)); // Peeking into FILE structure
+  }
+
+  // In this function, we assume bforartists format that has "o" and "mtllib" section
+  // and some number of "v" (vertices) and "f" (faces) sections
+
+  // Check for mtllib value
+  // Check for o (object) value
+
+  // All done - closing file
+  fclose(obj_file);
+  return FILE_OK;
+}
 
 int parse_options(int argc, char** argv, options_t* main_opt) {
   printf("Parsing options\n");
@@ -60,8 +98,13 @@ int main(int argc, char* argv[])
   int parse_ret_code = parse_options(argc, argv, main_options);
   if (parse_ret_code != OPTIONS_OK) return parse_ret_code;
 
+  // Everything is correct here - we can proceed
   // Check if filename is something that exists
   printf("main_options->input_file_name: %s\n", main_options->input_file_name);
+  int parse_file_ret = parse_file(main_options->input_file_name);
+
+  // Cleanup
+  free(main_options);
 
   return 0;
 }
